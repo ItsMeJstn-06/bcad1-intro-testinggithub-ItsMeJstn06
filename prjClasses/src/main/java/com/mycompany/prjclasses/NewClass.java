59bc1e670902fac768e50bb7765dc5570af48455
ItsMeJstn-06 <ST10470255@vcconnect.edu.za>
2025-03-11 10:51:35 +0200

    Added

---
+++ prjClasses/src/main/java/com/mycompany/prjclasses/NewClass.java
@@ -0,0 +1,19 @@
+package com.mycompany.prjclasses;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author justi
+ */
+public class NewClass {
+public String sSecondClass = "Testing";
+
+public void helloClass(){
+    System.out.println("Hello");
+}
+
+}
--- prjClasses/src/main/java/com/mycompany/prjclasses/PrjClasses.java
+++ prjClasses/src/main/java/com/mycompany/prjclasses/PrjClasses.java
@@ -11,6 +11,10 @@ package com.mycompany.prjclasses;
 public class PrjClasses {
 
     public static void main(String[] args) {
-        System.out.println("Hello World! \n I just did my first GitHub Test");
+        NewClass nc = new NewClass();
+        
+        nc.helloClass();
+        
+       System.out.println(nc.sSecondClass);
     }
 }
